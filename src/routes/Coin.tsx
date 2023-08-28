b67820966e36e17068378f025fbb97bd902f0c82
wonwoo <93998008+WonWoo-Shin@users.noreply.github.com>
2023-08-29 00:26:51 +0900

    2023.8.29

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+interface CoinParams {
+  coinId: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<CoinParams>();
+
+  return <h1>Coin: {coinId}</h1>;
+}
+
+export default Coin;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  textColor: "#f5f6fa",
+  bgColor: "#353b48",
+  accentColor: "#8c7ae6",
+};

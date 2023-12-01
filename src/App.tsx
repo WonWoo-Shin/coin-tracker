fdb337eb7a39c6feb629ba56d43714012ca1e40c
WonWoo-Shin <sww0481@gmail.com>
2023-12-02 00:06:57 +0900

    2023.12.02
    * 탭 아이콘, 텍스트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,24 @@ import { useRecoilValue } from "recoil";
 import { isDarkState, isSideActiveState } from "./atom";
 import SideMenu from "./components/SideMenu";
 import { GlobalStyle } from "./style/GlobalStyle";
+import { Helmet } from "react-helmet";
 
 function App() {
   const isDark = useRecoilValue(isDarkState);
   const isSideActive = useRecoilValue(isSideActiveState);
   return (
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <GlobalStyle />
-      <SideMenu />
-      <Main style={{ marginLeft: isSideActive ? "250px" : 0 }}>
-        <Router />
-      </Main>
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <title>오올블루</title>
+      </Helmet>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <SideMenu />
+        <Main style={{ marginLeft: isSideActive ? "250px" : 0 }}>
+          <Router />
+        </Main>
+      </ThemeProvider>
+    </>
   );
 }
 
